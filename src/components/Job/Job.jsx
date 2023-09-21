234f14155a2174c5286a53f12408f241a0b82652
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-21 22:01:29 +0600

    Set page layout and Display featured jobs

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,10 +1,26 @@
-const Job = ({job}) => {
-    const {logo} = job;
-    return (
+const Job = ({ job }) => {
+  const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
+  return (
+    <div className="card card-compact bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src={logo}
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{job_title}</h2>
+        <p>{company_name}</p>
         <div>
-            <img src={logo} alt="" />
+            <button className="px-5 py-2 text-[#7E90FE] font-extrabold border-lg mr-4 border-[#7E90FE]">{remote_or_onsite}</button>
+            <button className="px-5 py-2 text-[#7E90FE] font-extrabold border-lg mr-4 border-[#7E90FE]">{job_type}</button>
         </div>
-    );
+        <div className="card-actions">
+          <button className="btn btn-primary">View Details</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Job;
