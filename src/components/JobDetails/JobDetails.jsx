f1562f1cb0de8afa0ff8342ff9e278df5f8f243d
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-22 15:36:09 +0600

    Display applied jobs from stored ids

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -11,7 +11,7 @@ const JobDetails = () => {
     console.log(job);
 
     const handleApplyJob = () =>{
-        saveJobApplication(id);
+        saveJobApplication(idInt);
         toast('You have applied successfully')
     }
 
