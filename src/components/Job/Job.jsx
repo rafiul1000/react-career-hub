4e20e382f523a25f4f337b8653b152fa6de17a79
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-22 10:05:00 +0600

    Load job details page and display

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,8 +1,9 @@
 import { MdLocationOn } from "react-icons/md";
 import { HiOutlineCurrencyDollar } from "react-icons/hi2";
+import { Link } from "react-router-dom";
 
 const Job = ({ job }) => {
-  const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
+  const { id, logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
   return (
     <div className="card card-compact bg-base-100 shadow-xl">
       <figure>
@@ -24,7 +25,7 @@ const Job = ({ job }) => {
             
         </div>
         <div className="card-actions">
-          <button className="btn btn-primary">View Details</button>
+          <Link to={`/job/${id}`}><button className="btn btn-primary">View Details</button></Link>
         </div>
       </div>
     </div>
